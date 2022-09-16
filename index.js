a9ee7ff3338cde6346a1a84d5f1f8929bbb1f3e4
国星 孙 <tcguoxing@163.com>
2022-09-16 23:56:05 +0800

    main分支的第一次push

---
+++ index.js
@@ -0,0 +1,19 @@
+const rawArgv = process.argv.slice(2)
+const args = require('minimist')(rawArgv, {
+  boolean: [
+    // build
+    // FIXME: --no-module, --no-unsafe-inline, no-clean, etc.
+    'modern',
+    'report',
+    'report-json',
+    'inline-vue',
+    'watch',
+    // serve
+    'open',
+    'copy',
+    'https',
+    // inspect
+    'verbose'
+  ]
+})
+const command = args._[0]
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'none',
+    entry: './index.js',
+    output: {
+        filename: "vite-plugin-file.js"
+    }
+}
